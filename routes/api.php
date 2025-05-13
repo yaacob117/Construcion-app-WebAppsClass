de417963e7fec9191aebd630a672b94a72edc2ea
FerCocos <fertuli984@gmail.com>
2025-05-13 03:23:15 -0600

    Add FileApiController, OrderProductApiController and add the routes on /routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CustomerApiController;
+use App\Http\Controllers\Api\OrderProductApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,14 @@ Route::post('/apicustomers', [CustomerApiController::class, 'store']);
 Route::get('/apicustomers/{id}', [CustomerApiController::class, 'show']);
 Route::put('/apicustomers/{id}', [CustomerApiController::class, 'update']);
 Route::delete('/apicustomers/{id}', [CustomerApiController::class, 'destroy']);
-Route::get('/apicustomers/{customer}/order-status/{invoiceNumber}', [CustomerApiController::class, 'getOrderStatus']); 
+Route::get('/apicustomers/{customer}/order-status/{invoiceNumber}', [CustomerApiController::class, 'getOrderStatus']);
+
+Route::apiResource('order-products', 'App\Http\Controllers\Api\OrderProductApiController');
+Route::get('orders/{orderType}/{orderId}/products', 'App\Http\Controllers\Api\OrderProductApiController@getProductsByOrder');
+
+
+Route::post('upload', 'App\Http\Controllers\Api\FileApiController@upload');
+Route::get('download/{path}', 'App\Http\Controllers\Api\FileApiController@download')->where('path', '.*');
+Route::get('evidence/order/{orderId}', 'App\Http\Controllers\Api\FileApiController@getEvidenceByOrder');
+Route::get('evidence/{id}', 'App\Http\Controllers\Api\FileApiController@show');
+Route::delete('evidence/{id}', 'App\Http\Controllers\Api\FileApiController@destroy');
